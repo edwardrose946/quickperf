8352c25976bde5241c4d380d4a1eb474f7136f40
Edward Rose <exr194@cs.bham.ac.uk>
2020-05-18 14:21:16 +0100

    Pretty formatting of Jvm Profiling output.

--- jvm/jvm-annotations/src/main/java/org/quickperf/jvm/jmc/value/DisplayJvmProfilingValueVerifier.java
+++ jvm/jvm-annotations/src/main/java/org/quickperf/jvm/jmc/value/DisplayJvmProfilingValueVerifier.java
@@ -25,9 +25,7 @@ public class DisplayJvmProfilingValueVerifier implements
 
     private static final String LINE_SEPARATOR = System.lineSeparator();
 
-    private static final String LINE =
-        "------------------------------------------------------------------------------"
-            + LINE_SEPARATOR;
+    private static final String LINE = "------------------------------------------------------------------------------" + LINE_SEPARATOR;
 
 
     private DisplayJvmProfilingValueVerifier() {
@@ -79,38 +77,31 @@ public class DisplayJvmProfilingValueVerifier implements
 
         String text =
             LINE
-                + " ALLOCATION (estimations)" + "     |   " + "GARBAGE COLLECTION           "
-                + "|  THROWABLE" + LINE_SEPARATOR
-                + " Total       : " + fifteenLength.adapt(allocationTotal) + "|   "
-                + twentyNineLength.adapt("Total pause: " + totalGcPause) + "|  Exception: "
-                + exceptionsCount + LINE_SEPARATOR
-                + " Inside TLAB : " + fifteenLength.adapt(insideTlabSum) + "|   " + twentyNineLength
-                .adapt("Longest GC pause: " + gcPause) + "|  Error: " + errorCount + LINE_SEPARATOR
-                + " Outside TLAB: " + fifteenLength.adapt(outsideTlabSum) + "|   "
-                + twentyNineLength.adapt("") + "|  Throwable: " + throwablesCount + LINE_SEPARATOR
-                + " Allocation rate: " + twelveLength.adapt(allocationRate) + "|   "
-                + twentyNineLength.adapt("") + "|" + LINE_SEPARATOR
-                + LINE
-                + thirtyLength.adapt(" COMPILATION") + "|   " + "CODE CACHE" + LINE_SEPARATOR
-                + thirtyLength.adapt(" Number: " + compilationsCount) + "|   " + codeCacheFullCount
-                + LINE_SEPARATOR
-                + thirtyLength.adapt(" Longest: " + longestCompilation) + "|   " + LINE_SEPARATOR
-                + LINE
-                + " " + "JVM" + LINE_SEPARATOR
-                + " Name: " + jvmName + LINE_SEPARATOR
-                + " Version: " + jvmVersion + LINE_SEPARATOR
-                + " Arguments: " + jvmArguments + LINE_SEPARATOR
-                + LINE
-                + " " + "HARDWARE" + LINE_SEPARATOR
-                + " Hardware threads: " + minHwThreads + LINE_SEPARATOR
-                + " Cores: " + minNumberOfCores + LINE_SEPARATOR
-                + " Sockets: " + minNumberOfSockets + LINE_SEPARATOR
-                + " CPU: " + LINE_SEPARATOR
-                + cpuDescription + LINE_SEPARATOR
-                + LINE
-                + " OS:" + LINE_SEPARATOR
-                + osVersion + LINE_SEPARATOR
-                + LINE;
+            + " ALLOCATION (estimations)"                          + "     |   " + "GARBAGE COLLECTION           "                               + "|  THROWABLE" + LINE_SEPARATOR
+            + " Total       : " + fifteenLength.adapt(allocationTotal)  + "|   " + twentyNineLength.adapt("Total pause: " + totalGcPause) + "|  Exception: " + exceptionsCount + LINE_SEPARATOR
+            + " Inside TLAB : " + fifteenLength.adapt(insideTlabSum)    + "|   " + twentyNineLength.adapt("Longest GC pause: " + gcPause) + "|  Error: " + errorCount + LINE_SEPARATOR
+            + " Outside TLAB: " + fifteenLength.adapt(outsideTlabSum)   + "|   " + twentyNineLength.adapt("")                             + "|  Throwable: " + throwablesCount + LINE_SEPARATOR
+            + " Allocation rate: " + twelveLength.adapt(allocationRate) + "|   " + twentyNineLength.adapt("")                             + "|" + LINE_SEPARATOR
+            + LINE
+            + thirtyLength.adapt(" COMPILATION")                    + "|   " + "CODE CACHE" + LINE_SEPARATOR
+            + thirtyLength.adapt(" Number: " + compilationsCount)   + "|   " + codeCacheFullCount + LINE_SEPARATOR
+            + thirtyLength.adapt(" Longest: " + longestCompilation) + "|   " + LINE_SEPARATOR
+            + LINE
+            + " " + "JVM" + LINE_SEPARATOR
+            + " Name: " + jvmName + LINE_SEPARATOR
+            + " Version: " + jvmVersion + LINE_SEPARATOR
+            + " Arguments: " + jvmArguments + LINE_SEPARATOR
+            + LINE
+            + " " + "HARDWARE" + LINE_SEPARATOR
+            + " Hardware threads: " + minHwThreads + LINE_SEPARATOR
+            + " Cores: " + minNumberOfCores + LINE_SEPARATOR
+            + " Sockets: " + minNumberOfSockets + LINE_SEPARATOR
+            + " CPU: " + LINE_SEPARATOR
+            + cpuDescription + LINE_SEPARATOR
+            + LINE
+            + " OS:" + LINE_SEPARATOR
+            + osVersion + LINE_SEPARATOR
+            + LINE;
 
         System.out.println(text);
 
